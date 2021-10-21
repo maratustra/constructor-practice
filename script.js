9e45acefe9db26671db6fbd178a764d4e2093c01
Alena Maramygina <maratustraa@gmail.com>
2021-10-21 21:19:06 +0500

    создана функция-конструктор DomElement

---
+++ script.js
@@ -0,0 +1,35 @@
+// Внутрь созданного блока записывать любой текст. Метод записи может быть любым.
+let area = document.getElementById('area');
+
+const DomElement = function (selector, height, width, bg, fontSize) {
+  this.selector = selector;
+  this.height = height;
+  this.width = width;
+  this.bg = bg;
+  this.fontSize = fontSize;
+};
+
+DomElement.prototype.createElem = function () {
+  if (this.selector.startsWith('.')) {
+    let newDiv = document.createElement('div');
+    newDiv.className = `${this.selector.substring(1)}`;
+    newDiv.style.cssText = `height: ${this.height}px; width: ${this.width}px; background: ${this.bg}; font-size: ${this.fontSize}px;`;
+    newDiv.textContent = 'Привет, новый класс!';
+    area.append(newDiv);
+  } else if (this.selector.startsWith('#')) {
+    let newParag = document.createElement('p');
+    newParag.id = `${this.selector.substring(1)}`;
+    newParag.style.cssText = `height: ${this.height}px; width: ${this.width}px; background: ${this.bg}; font-size: ${this.fontSize}px;`;
+    newParag.textContent = 'Здорово, новый айди!';
+    area.append(newParag);
+  }
+};
+
+let newDomClass = new DomElement('.square', 500, 500, 'red', 50);
+newDomClass.createElem();
+
+let newDomId = new DomElement('#crazy', 100, 250, 'green', 20);
+newDomId.createElem();
+
+console.log(newDomClass);
+console.log(newDomId);
